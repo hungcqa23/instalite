61c0cc414b8d0d04be66eb259d178c391d6f7333
An Hung <anbeel191@gmail.com>
2024-02-28 15:22:41 +0700

    style: add header (component)

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,3 +1,8 @@
+import Image from 'next/image';
 export default function Header() {
-  return <header>header</header>;
+  return (
+    <header className='h-16 w-full bg-gray-50'>
+      <Image src='/orbit.svg' alt='logo' width={56} height={56} />
+    </header>
+  );
 }
