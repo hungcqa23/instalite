a3f5f18af5a223f2813c87948c566f50465d6bf8
An Hung <anbeel191@gmail.com>
2024-03-01 09:20:13 +0700

    style: add style for header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Home,
   Search,
@@ -8,33 +10,47 @@ import {
   AlignRightIcon
 } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
+import Popover from './popover';
+
 export default function Header() {
   return (
-    <header className='flex h-16 w-full items-center justify-center gap-28 bg-gray-50'>
-      <Image src='/orbit.svg' alt='logo' width={46} height={46} />
+    <header className='flex h-16 w-full items-center justify-center gap-32'>
+      <Link href={'/'}>
+        <Image src='/orbit.svg' alt='logo' width={46} height={46} />
+      </Link>
+
       <div className='flex gap-2'>
-        <button className='flex h-16 w-24 items-center justify-center rounded-lg bg-gray-100 hover:bg-gray-200'>
-          <Home className='h-7 w-7 text-zinc-400' />
+        <button className='duration-400 flex h-16 w-24 items-center justify-center rounded-lg text-zinc-400 transition-colors hover:bg-gray-200 hover:text-black'>
+          <Home className='h-7 w-7 ' />
         </button>
-        <button className='flex h-16 w-24 items-center justify-center rounded-lg bg-gray-100 hover:bg-gray-200'>
-          <Search className='h-7 w-7 text-zinc-400' />
+        <button className='duration-400 flex h-16 w-24 items-center justify-center rounded-lg text-zinc-400 transition-colors hover:bg-gray-200 hover:text-black'>
+          <Search className='h-7 w-7 ' />
         </button>
-        <button className='flex h-16 w-24 items-center justify-center rounded-lg bg-gray-100 hover:bg-gray-200'>
-          <SquarePen className='h-7 w-7 text-zinc-400' />
+        <button className='duration-400 flex h-16 w-24 items-center justify-center rounded-lg text-zinc-400 transition-colors hover:bg-gray-200 hover:text-black'>
+          <SquarePen className='h-7 w-7 ' />
         </button>
-        <button className='flex h-16 w-24 items-center justify-center rounded-lg bg-gray-100 hover:bg-gray-200'>
-          <Bell className='h-7 w-7 text-zinc-400' />
+        <button className='duration-400 flex h-16 w-24 items-center justify-center rounded-lg text-zinc-400 transition-colors hover:bg-gray-200 hover:text-black'>
+          <Bell className='h-7 w-7 ' />
         </button>
-        <button className='flex h-16 w-24 items-center justify-center rounded-lg bg-gray-100 hover:bg-gray-200'>
-          <UserRound className='h-7 w-7 text-zinc-400' />
+        <button className='duration-400 flex h-16 w-24 items-center justify-center rounded-lg text-zinc-400 transition-colors hover:bg-gray-200 hover:text-black'>
+          <UserRound className='h-7 w-7 ' />
         </button>
-        <button className='flex h-16 w-24 items-center justify-center rounded-lg bg-gray-100 hover:bg-gray-200'>
-          <MessageCircleHeartIcon className='h-7 w-7 text-zinc-400' />
+        <button className='duration-400 flex h-16 w-24 items-center justify-center rounded-lg text-zinc-400 transition-colors hover:bg-gray-200 hover:text-black'>
+          <MessageCircleHeartIcon className='h-7 w-7 ' />
         </button>
       </div>
-      <button className='duration-400 flex h-8 w-8 items-center justify-center rounded-lg text-zinc-400 transition-colors hover:text-black'>
-        <AlignRightIcon className='h-8 w-8' />
-      </button>
+
+      <Popover
+        placement='bottom-end'
+        renderPopover={
+          <ul className='shadow-default h-52 w-60 rounded-xl'></ul>
+        }
+      >
+        <button className='duration-400 flex h-8 w-8 items-center justify-center rounded-lg text-zinc-400 transition-colors hover:text-black'>
+          <AlignRightIcon className='h-8 w-8' />
+        </button>
+      </Popover>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,9 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out'
       }
+    },
+    boxShadow: {
+      default: '0 0 10px rgba(0, 0, 0, 0.1)'
     }
   },
   plugins: [require('tailwindcss-animate'), 'prettier-plugin-tailwindcss']
