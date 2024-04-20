9aa803a83d8a0d01c66520701fb33418958bb78f
Ly Thanh Tu Anh <tuanhlythanh@gmail.com>
2024-04-20 23:50:23 +0700

    feat: dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
 import Header from '@/components/ui/header';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -19,12 +20,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body
         className={`${inter.className} overflow-x-hidden overflow-y-hidden`}
       >
-        {/* <Header /> */}
-        {children}
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
