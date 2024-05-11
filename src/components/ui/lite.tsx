723fcbec55c2cd84435a6d8396993397ce38ec26
hungcqa23 <anbeel191@gmail.com>
2024-05-11 22:14:05 +0700

    feat(lite): add fix ui lite

--- src/components/ui/lite.tsx
+++ src/components/ui/lite.tsx
@@ -1,7 +1,7 @@
 import { Lite } from '@/types/lite.type';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import { Ellipse } from 'lucide-react';
+import { LucideExternalLink } from 'lucide-react';
 
 export default function LiteItem({ lite }: { lite: Lite }) {
   return (
@@ -18,7 +18,22 @@ export default function LiteItem({ lite }: { lite: Lite }) {
           </div>
         </div>
         <Button variant={'link'}>
-          <Ellipsis />
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            stroke-width='2'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            className='lucide lucide-ellipsis'
+          >
+            <circle cx='12' cy='12' r='1' />
+            <circle cx='19' cy='12' r='1' />
+            <circle cx='5' cy='12' r='1' />
+          </svg>
         </Button>
       </div>
     </div>
