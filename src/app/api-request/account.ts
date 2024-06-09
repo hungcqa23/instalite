2b01a356b19d8ed540c9edbb8be382ac8620b84f
hungcqa23 <94952913+hungcqa23@users.noreply.github.com>
2024-06-09 21:03:58 +0700

    add

--- src/app/api-request/account.ts
+++ src/app/api-request/account.ts
@@ -3,5 +3,9 @@ import { http } from '@/lib/http';
 export const accountApiRequest = {
   me: () => http.get('/users/me'),
   search: (username: string) => http.get(`/users?username=${username}`),
-  recommend: () => http.get(`/users/recommend`)
+  recommend: () => http.get(`/users/recommend`),
+  follow: (followedUserId: string) =>
+    http.post('/users/follow', {
+      followedUserId
+    })
 };
