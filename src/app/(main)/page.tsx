383109c07d1a90f735e0a6c0a3d760358ee9fd7a
hungcqa23 <94952913+hungcqa23@users.noreply.github.com>
2024-06-10 10:11:14 +0700

    add file

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,31 +1,10 @@
 import SuggestedBar from '@/app/(main)/ui/SuggestedBar';
 import CreateLiteDialog from '@/components/ui/create-lite-dialog';
 import IconProfile from '@/components/ui/icon-profile';
-import LiteItem from '@/components/ui/lite-item';
-import { Lite } from '@/types/lite.type';
 
-const testLite: Lite = {
-  avatar:
-    'https://scontent.cdninstagram.com/v/t51.29350-15/447077438_978175286926900_4717450693372967518_n.jpg?stp=dst-jpg_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYyOTM1MCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=hUv-Cemq_nUQ7kNvgHN17WZ&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzM4MDE5MjkxMTcyNjk0NTQxNw%3D%3D.2-ccb7-5&oh=00_AYDBpLiL2Uw0v8Xxe3fcIyA3NwI1JzlTYsau_NeVAPfD-w&oe=6660E307&_nc_sid=10d13b',
-  content:
-    'Aenean vitae porttitor magna, non venenatis tortor. In hac habitasse platea dictumst. Morbi sagittis, ante et condimentum malesuada, lectus arcu faucibus urna, eget eleifend ipsum metus et ligula.',
-  createdAt: 'a hour ago',
-  likesCount: 1600,
-  replysCount: 2,
-  username: 'anhung_dep_try'
-};
-const testLite1: Lite = {
-  avatar:
-    'https://scontent-iad3-1.cdninstagram.com/v/t51.29350-15/446592648_1922152721578822_1948576158759031871_n.jpg?stp=dst-jpegr_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDI1NjAuaGRyLmYyOTM1MCJ9&_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=110&_nc_ohc=07VHeFcx0lgQ7kNvgGK-VQT&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzM4MDI0Njg4ODUzNDkwODU4OQ%3D%3D.2-ccb7-5&oh=00_AYA_70zWxvwXP9Jhbvj5o0T2kP0HcSpHJBOEe-denvOc0g&oe=665EDC89&_nc_sid=10d13b',
-  content:
-    'Aenean vitae porttitor magna, non venenatis tortor. In hac habitasse platea dictumst. Morbi sagittis, ante et condimentum malesuada, lectus arcu faucibus urna, eget eleifend ipsum metus et ligula.',
-  createdAt: 'a hour ago',
-  likesCount: 1067,
-  replysCount: 2,
-  username: 'anhung_dep_try',
-  url: 'https://images.unsplash.com/photo-1717201410616-205a82d7e3f9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-};
 export default function Home() {
+  // const
+
   return (
     <>
       {/* Main concept list xl:mr-[calc((100%-30rem)/2-16rem)] */}
