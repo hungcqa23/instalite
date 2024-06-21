daf05f4e91c2906d81e4bf5ece9eb7350adaaacc
Ly Thanh Tu Anh <tuanhlythanh@gmail.com>
2024-06-21 23:22:57 +0700

    feat(follower/following list): add follower/following list

--- src/app/(main)/username/[username]/tab.tsx
+++ src/app/(main)/username/[username]/tab.tsx
@@ -21,25 +21,15 @@ export default function Tab({
     <div className='mt-[31px]'>
       <Tabs defaultValue='post' className='w-full'>
         <TabsList className='flex w-full justify-between rounded-none border-b bg-transparent p-0 dark:bg-transparent'>
-          <Link href='/me'>
-            <TabsTrigger
-              className='shadow-none data-[state=active]:shadow-none  relative flex h-9 w-32 gap-1.5 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 text-base font-semibold uppercase text-muted-foreground transition-none data-[state=active]:border-b-primary data-[state=active]:text-foreground'
-              value='post'
-            >
-              <Grid2X2 className='h-8 w-8 md:h-6 md:w-6' />{' '}
-              <span className='hidden md:inline'>Lite</span>
-            </TabsTrigger>
-          </Link>
-
           <TabsTrigger
             className='shadow-none data-[state=active]:shadow-none  relative flex h-9 w-32 gap-1.5 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 text-base font-semibold uppercase text-muted-foreground transition-none data-[state=active]:border-b-primary data-[state=active]:text-foreground'
-            value='image'
+            value='post'
           >
-            <ImageIcon className='h-8 w-8 md:h-6 md:w-6' />{' '}
-            <span className='hidden md:inline'>Image</span>
+            <Grid2X2 className='h-8 w-8 md:h-6 md:w-6' />{' '}
+            <span className='hidden md:inline'>Lite</span>
           </TabsTrigger>
 
-          <Link href='/me/saved'>
+          {/* {user?.username === currentUser?.username && (
             <TabsTrigger
               className='shadow-none data-[state=active]:shadow-none relative flex h-9 w-32 gap-1.5 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 text-base font-semibold uppercase text-muted-foreground transition-none data-[state=active]:border-b-primary data-[state=active]:text-foreground'
               value='saved'
@@ -47,7 +37,14 @@ export default function Tab({
               <Bookmark className='h-8 w-8 md:h-6 md:w-6' />
               <span className='hidden md:inline'>Saved</span>
             </TabsTrigger>
-          </Link>
+          )} */}
+          <TabsTrigger
+            className='shadow-none data-[state=active]:shadow-none relative flex h-9 w-32 gap-1.5 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 text-base font-semibold uppercase text-muted-foreground transition-none data-[state=active]:border-b-primary data-[state=active]:text-foreground'
+            value='saved'
+          >
+            <Bookmark className='h-8 w-8 md:h-6 md:w-6' />
+            <span className='hidden md:inline'>Saved</span>
+          </TabsTrigger>
         </TabsList>
 
         <TabsContent value='post' className='text-sm'>
@@ -162,15 +159,14 @@ export default function Tab({
           obcaecati minima odit quidem quam suscipit.
         </TabsContent>
 
-        <TabsContent value='image' className='w-full max-w-full text-sm'>
-          <div className='w-full'></div>
-        </TabsContent>
-
-        {user.username === currentUser?.username && (
+        {/* {user?.username === currentUser?.username && (
           <TabsContent value='saved' className='text-sm'>
             Saved
           </TabsContent>
-        )}
+        )} */}
+        <TabsContent value='saved' className='text-sm'>
+          Saved
+        </TabsContent>
       </Tabs>
     </div>
   );
--- src/components/ui/follow-user-item.tsx
+++ src/components/ui/follow-user-item.tsx
@@ -1,13 +1,10 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import { Account } from '@/schema-validations/account.schema';
+import { Account, FollowUser } from '@/schema-validations/account.schema';
 import { User } from '@/types/user.type';
 import Link from 'next/link';
-export default function UserItem({ user }: { user: Account }) {
-  user.full_name = user.full_name || 'Instalite User';
-  user.avatar =
-    user.avatar ||
-    'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+import React from 'react';
+export default function FollowUserItem({ user }: { user: FollowUser }) {
   return (
     <Link
       href={`/username/${user.username}`}
@@ -24,17 +21,17 @@ export default function UserItem({ user }: { user: Account }) {
             {user.full_name}
           </div>
           {/* <div className='mt-1 text-sm font-normal'>
-            {user.followersCount} followers
-          </div> */}
+        {user.followersCount} followers
+      </div> */}
         </div>
         {/* <div className='flex flex-col justify-center '>
-          <Button
-            className='mt-1 h-8 rounded-lg px-7 text-sm hover:bg-transparent dark:bg-zinc-950 dark:hover:bg-zinc-950'
-            variant={'outline'}
-          >
-            Follow
-          </Button>
-        </div> */}
+      <Button
+        className='mt-1 h-8 rounded-lg px-7 text-sm hover:bg-transparent dark:bg-zinc-950 dark:hover:bg-zinc-950'
+        variant={'outline'}
+      >
+        Follow
+      </Button>
+    </div> */}
       </div>
     </Link>
   );
