0b050ce67519392529c54b0168a6c7ccde7925c7
Ly Thanh Tu Anh <tuanhlythanh@gmail.com>
2024-06-22 11:00:54 +0700

    feat(upload avatar): add upload avatar function

--- src/components/ui/user-avatar.tsx
+++ src/components/ui/user-avatar.tsx
@@ -2,9 +2,32 @@
 
 import { useAppContext } from '@/app/context/app-context';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import React from 'react';
+import { Account } from '@/schema-validations/account.schema';
+import { useQuery } from '@tanstack/react-query';
+import { getCookie } from 'cookies-next';
+import React, { useState } from 'react';
 export default function UserAvatar() {
-  const { user } = useAppContext();
+  const accessToken = getCookie('access_key');
+  const [user, setUser] = useState<Account>();
+  const { data } = useQuery({
+    queryKey: ['users', accessToken],
+    queryFn: async () => {
+      const res = await fetch('http://localhost:8000/users/me', {
+        method: 'GET',
+        headers: {
+          Cookie: `access_token=${accessToken}`
+        },
+        credentials: 'include'
+      });
+
+      const data = await res.json();
+      const { user }: { user: Account } = data;
+      setUser(user);
+
+      return data;
+    }
+  });
+
   return (
     <Avatar className='mt-0.5 h-8 w-8'>
       <AvatarImage src={user?.avatar} alt='@shadcn' />
